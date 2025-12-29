ead884e6e27e9a41762f153aa4bb0805a275d604
David Op de Beeck <davidopdebeeck@outlook.be>
2025-12-30 00:59:08 +0100

    refactor(session): remove unused imports from DeckView

--- guessimate-session/application/src/main/java/app/dodb/guessimate/session/drivenadapter/deckview/DeckView.java
+++ guessimate-session/application/src/main/java/app/dodb/guessimate/session/drivenadapter/deckview/DeckView.java
@@ -1,10 +1,7 @@
 package app.dodb.guessimate.session.drivenadapter.deckview;
 
 import app.dodb.guessimate.session.api.deck.DeckTO;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import org.hibernate.annotations.JdbcTypeCode;
