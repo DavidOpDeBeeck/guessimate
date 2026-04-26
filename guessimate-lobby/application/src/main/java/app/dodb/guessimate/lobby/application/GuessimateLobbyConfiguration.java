92ce1356a19e2528c480277bfce7e9c6e20b19f3
David Op de Beeck <davidopdebeeck@outlook.be>
2026-04-26 13:17:17 +0200

    refactor: remove commented-out transaction manager code

--- guessimate-lobby/application/src/main/java/app/dodb/guessimate/lobby/application/GuessimateLobbyConfiguration.java
+++ guessimate-lobby/application/src/main/java/app/dodb/guessimate/lobby/application/GuessimateLobbyConfiguration.java
@@ -3,13 +3,9 @@ package app.dodb.guessimate.lobby.application;
 import app.dodb.guessimate.lobby.drivenadapter.GuessimateLobbyDrivenAdapterConfiguration;
 import app.dodb.guessimate.lobby.drivingadapter.GuessimateLobbyDrivingAdapterConfiguration;
 import app.dodb.guessimate.lobby.usecase.GuessimateLobbyUseCaseConfiguration;
-import jakarta.persistence.EntityManagerFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.orm.jpa.JpaTransactionManager;
 
 @Configuration
 @Import({
@@ -20,9 +16,4 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 @ComponentScan
 public class GuessimateLobbyConfiguration {
 
-//    @Bean(name = "transactionManager")
-//    @ConditionalOnMissingBean(JpaTransactionManager.class)
-//    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-//        return new JpaTransactionManager(entityManagerFactory);
-//    }
 }
--- guessimate-session/application/src/main/java/app/dodb/guessimate/session/application/GuessimateSessionConfiguration.java
+++ guessimate-session/application/src/main/java/app/dodb/guessimate/session/application/GuessimateSessionConfiguration.java
@@ -3,12 +3,8 @@ package app.dodb.guessimate.session.application;
 import app.dodb.guessimate.session.drivenadapter.GuessimateDrivenAdapterConfiguration;
 import app.dodb.guessimate.session.drivingadapter.GuessimateDrivingAdapterConfiguration;
 import app.dodb.guessimate.session.usecase.GuessimateUseCaseConfiguration;
-import jakarta.persistence.EntityManagerFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.orm.jpa.JpaTransactionManager;
 
 @Configuration
 @Import({
@@ -18,9 +14,4 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 })
 public class GuessimateSessionConfiguration {
 
-//    @Bean(name = "transactionManager")
-//    @ConditionalOnMissingBean(JpaTransactionManager.class)
-//    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-//        return new JpaTransactionManager(entityManagerFactory);
-//    }
 }
